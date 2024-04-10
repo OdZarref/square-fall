3484ecc3e9c786ee0a0a9db3ce9292a5ae3845d7
OdZarref <ludwigvonmarcelo@gmail.com>
2024-04-10 07:17:20 -0300

    Adding reset on collision to walls and point when passing by walls

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,24 +1,34 @@
 extends Node2D
 
-const MAXIMUM_WALLS = 14
+var spawn_walls = true
+var total_walls : int = 0
+var last_wall_pos = 0
+var points = 0
+const MAXIMUM_WALLS = 7
 const WALLS_PRELOAD = preload("res://scenes/walls.tscn")
-var total_walls = 0
-var last_wall_position : int
+const SPACE_BETWEEN_WALLS = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	while total_walls < MAXIMUM_WALLS:
-		spawn_wall()
+	for i in range(0, MAXIMUM_WALLS):
+		total_walls += 1
+		spawn_wall(last_wall_pos + SPACE_BETWEEN_WALLS)
+		
+		
+func reset():
+	total_walls = 0
+	last_wall_pos = 0
 
-func spawn_wall():
-	const SPACE_BETWEEN_WALLS = 400
+func spawn_wall(posx):
 	var wall_instance = WALLS_PRELOAD.instantiate()
+	wall_instance.position.y = randi_range(-200, 200)
+	wall_instance.position.x = posx + SPACE_BETWEEN_WALLS
+	last_wall_pos = wall_instance.position.x
+
 	
-	wall_instance.position.y = randi_range(-100, 100)
-	wall_instance.position.x = last_wall_position + SPACE_BETWEEN_WALLS
-	last_wall_position = wall_instance.position.x
-	self.add_child(wall_instance)
-	total_walls += 1
+	self.call_deferred('add_child', wall_instance)
+	#self.add_child(wall_instance)
+	print('new wall added')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,8 +37,14 @@ func _process(delta):
 
 
 func _on_resetter_area_entered(area):
-	print(total_walls)
+	print('wall deleted')
 	area.get_parent().queue_free()
-	total_walls -= 1
-	print(total_walls)
-	spawn_wall()
+	spawn_walls = false
+	spawn_wall(1150)
+
+func _on_point_detector_area_entered(area):
+	if area.name == 'CenterDetector':
+		points += 1
+		print(points)
+	else:
+		get_tree().reload_current_scene()
